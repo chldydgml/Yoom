903e2ec61ad01fb8acc8ca28ea0f7c345c39da57
chldydgml09 <chldydgml09@naver.com>
2023-11-18 16:50:11 +0900

    ✨Feat : #2.3 Recap

--- src/server.js
+++ src/server.js
@@ -19,11 +19,11 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", socket => {
-    socket.on("enter_room", (msg, done) => {
-        console.log(msg);
+    socket.on("enter_room", (roomName, done) => {
+        console.log(roomName);
         setTimeout(() => {
-            done();
-        }, 5000);
+            done("hello from the backend");
+        }, 5000)
     });
 })
 
